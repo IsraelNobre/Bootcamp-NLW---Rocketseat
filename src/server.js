85d8430d6683de0d1603e0f07aa656a6c6217a8a
Israel Nobre Oliveira da Silva <nobreisrael@gmail.com>
2020-08-08 11:36:19 -0300

    Server funcionando localmente

--- src/server.js
+++ src/server.js
@@ -1,43 +1,46 @@
 const proffys = [
-  {
-    name:"Diego Fernandes" ,
-    avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4" , whatsapp:"9888218282" ,
-    bio:'Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.' ,
-    subject:"Química" , 
-    cost: "20",
-    weekday:[0] ,
-    time_from: [720], 
-    time_to:[1220 ],
+      {
+      name:"Diego Fernandes",
+      avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+      whatsapp:"9888218282",
+      bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+      subject:"Química" , 
+      cost: "20",
+      weekday:[0] ,
+      time_from: [720], 
+      time_to:[1220 ],
 
-  }
-  {
-    name:"Daniele Evangelista" ,
-    avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4" , whatsapp:"9888218282" ,
-    bio:'Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.' ,
-    subject:"Química" , 
-    cost: "20",
-    weekday:[1] ,
-    time_from: [720], 
-    time_to:[1220 ],
+    },
+     {
+      name:"Daniele Evangelista" ,
+      avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4" , whatsapp:"9888218282" ,
+      bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+      subject:"Química", 
+      cost: "20",
+      weekday:[1] ,
+      time_from: [720], 
+      time_to:[1220],
 
-  }
+    }
 ]
 function pageLanding(req, res){
-  return res.sendFile(__dirname + "/views/index.html")
+  return res.render("index.html")
 }
 function pageStudy(req, res){
-  return res.sendFile(__dirname + "/views/study.html")
+  return res.render("study.html",{proffys})
 }
-function pageGiveClasses\'[(req, res){
-  return res.sendFile(__dirname + "/views/give-classes.html")
+function pageGiveClasses(req, res){
+  return res.render("give-classes.html")
 }
 
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
+
 //configurar nunjucks
-nunjucks.configure('src/views'{
-  express: ser
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views',{
+  express: server,
+  noCache:true,
 })
 
 server
